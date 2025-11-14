72773a0a1b8b93dde2e2bb3f17bb4f1d0f45e08c
SARAVANAKUMAR_CCL <saravanakumar.m@crystalcodelabs.com>
2025-11-14 13:06:13 +0530

    a

--- src/Components/DailyCollection.jsx
+++ src/Components/DailyCollection.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../Styles/DailyCollection.css";
 
